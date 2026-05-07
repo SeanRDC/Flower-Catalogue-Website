bf619c6d71819cc500ad0c318d5c89997e5c98b1
Sean Rhani Dela Cruz <delacruzseanrhani@gmail.com>
2026-05-08 04:55:34 +0800

    Add frontend libraries

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Browse from './pages/Browse';
+import Footer from './components/Footer';
+import './styles/global.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/browse" element={<Browse />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
