d1262d4507be1fbc0a8faf11b0cd375d9a932b15
SeanRDC <delacruzseanrhani@gmail.com>
2026-05-19 11:55:17 +0800

    removed typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';s
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; 
 import AuthModal from './components/AuthModal';       
