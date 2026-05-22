cf266f583bc9c49a84ed0edcaa42426114888f62
SeanRDC <delacruzseanrhani@gmail.com>
2026-05-22 22:27:30 +0800

    Fixed ENETUNREACH error

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,10 @@ const router = express.Router();
 const otpStore = new Map();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,          
+  secure: false,      
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD
