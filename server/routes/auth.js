f408716ba4fee1eca134e6341249a438b45ea36f
SeanRDC <delacruzseanrhani@gmail.com>
2026-05-22 22:34:39 +0800

    forceto ipv4

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,7 +13,8 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD
-  }
+  },
+  family: 4
 });
 
 router.post('/send-otp', async (req, res) => {
