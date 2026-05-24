e4dda264819f6b4c0a20ccfe69b400a159c5d8ff
Sean Rhani Dela Cruz <delacruzseanrhani@gmail.com>
2026-05-24 12:41:56 +0000

    updated schema

--- server/models/Flower.js
+++ server/models/Flower.js
@@ -7,7 +7,11 @@ const flowerSchema = new mongoose.Schema({
   lifecycle: { type: String, enum: ['Annual', 'Biennial', 'Perennial'] },
   description: { type: String },
   imageUrl: { type: String, required: true },
-  tags: [String]
+  tags: [String],
+  color: { type: String },
+  petalShape: { type: String },
+  type: { type: String },
+  symbolism: { type: String }
 });
 
 export default mongoose.model('Flower', flowerSchema);
